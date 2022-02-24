dc0f1731f2e269209a25ddc591ae977709efc002
Shivam Kumar <78433942+shivam2003-dev@users.noreply.github.com>
2022-02-24 12:25:05 +0530

    Time: 121 ms (63.68%), Space: 30.2 MB (66.97%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -8,46 +8,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if (!head || !head->next)
-            return head;
-        ListNode* mid = getMid(head);
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(mid);
-        return merge(left, right);
-    }
-
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode dummyHead(0);
-        ListNode* ptr = &dummyHead;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                ptr->next = list1;
-                list1 = list1->next;
-            } else {
-                ptr->next = list2;
-                list2 = list2->next;
-            }
-            ptr = ptr->next;
+     ListNode* sortList(ListNode* head) {
+        vector<int> ans;
+        ListNode *dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode *temp=head;
+        while(temp){
+            ans.push_back(temp->val);
+            temp=temp->next;
         }
-        if(list1) ptr->next = list1;
-        else ptr->next = list2;
-
-        return dummyHead.next;
-    }
-
-    ListNode* getMid(ListNode* head) {
-        ListNode* midPrev = nullptr;
-        while (head && head->next) {
-            midPrev = (midPrev == nullptr) ? head : midPrev->next;
-            head = head->next->next;
+        sort(begin(ans),end(ans));
+        for(auto i:ans){
+            head->val=i;
+            head=head->next;
         }
-        ListNode* mid = midPrev->next;
-        midPrev->next = nullptr;
-        return mid;
+        return dummy->next;
     }
 };
