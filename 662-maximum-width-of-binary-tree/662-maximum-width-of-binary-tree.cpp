7923ae6ac42f5cbdbb7fcdbd0d425d353a41e209
Shivam Kumar <78433942+shivam2003-dev@users.noreply.github.com>
2022-02-28 00:46:16 +0530

    Time: 10 ms (69.33%), Space: 17.3 MB (79.50%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,45 +12,27 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        
-        int res = 1;
-        queue<pair<TreeNode*, int>> q;
-        
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
-        {
-            int cnt = q.size();
-            // start is the index of root node for first level
-            int start = q.front().second;
-            int end = q.back().second;
-            
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
-            {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                int idx = p.second - start;
-                
+        int width = 0;
+        queue<pair<TreeNode*, int>>q;
+        q.push({root, 0});
+        while(!q.empty()){
+            int n = q.size();
+            int max_index = INT_MIN, min_index = INT_MAX;
+            while(n--){
+                TreeNode* tmp = q.front().first;
+                int dist = q.front().second;
                 q.pop();
-                
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
-                
-                // if right child exist
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
+                max_index = max(max_index, dist);
+                min_index = min(min_index, dist);
+                if(tmp->left){
+                    q.push({tmp->left, (long long)2 * dist + 1});
+                }
+                if(tmp->right){
+                    q.push({tmp->right, (long long)2 * dist + 2});
+                }
             }
+            width = max(width, max_index - min_index + 1);
         }
-        
-        return res;
-        
+        return width;   
     }
 };
