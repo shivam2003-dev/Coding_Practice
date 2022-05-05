6493b07595426eace7a911727a5db5bef915943a
Shivam Kumar <78433942+shivam2003-dev@users.noreply.github.com>
2022-05-05 07:42:16 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (19.21%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,58 @@
+class MyStack {
+public:
+    queue<int> q;
+    queue<int> p;
+    
+    MyStack() {
+       
+    }
+    
+    void push(int x) {
+         
+        q.push(x);            //   simply push the element
+    }
+    
+    int pop() {
+        
+        while(q.size()!=1){       // push all other elements in the other queue except the last one
+            p.push(q.front());
+            q.pop();
+        }
+        
+        int y = q.front();
+        q.pop();                  // pop the last element but dont put it in other queue
+        
+        while(p.size()!=0){
+            q.push(p.front());     //  put all other elements back in first queue
+            p.pop();
+        }
+        
+        return y;
+        
+    }
+    
+    int top() {
+        
+        int k = pop();          // top can be seen as just a pop function followed by a push 
+                                // pop gives last element and remove it from queue so if we put it back , we can get the functionality
+        q.push(k);
+        
+        
+        return k;
+        
+    }
+    
+    bool empty() {
+        
+        return q.size()==0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
